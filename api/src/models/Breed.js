d781737340975e921dda7c5db452f944981f44ac
Dionicio Hurtado <hurtado82@gmail.com>
2022-09-07 19:42:48 -0500

    first step with routes and models

---
+++ api/src/models/Breed.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize")
+
+module.exports = (sequelize) => {
+  sequelize.define("breed", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+    },
+  });
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require("axios") 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +9,53 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const breedsByName = (breed) => {
+ return breed.map((b) => b.name);
+}
+
+router.get("/dogs", (req, res) => {
+  const {name} = req.query;
+  console.log(name)
+  axios
+    .get("https://api.thedogapi.com/v1/breeds")
+    .then((data) => data.data)
+    .then((breeds) => {
+      if (name) {
+        const nameSearch = breeds.filter( (b) => b.name.toLowerCase().startsWith(name.toLowerCase()) === true );
+        if (!nameSearch.length) return res.status(404).send("Breed no found");
+        else {
+          res.status(200).send(nameSearch);
+        }
+
+      } else {
+        const breedName = breedsByName(breeds);
+        res.status(200).send(breedName);
+      }
+    });
+});
+
+router.get("/dogs/:id",(req, res) => {
+  const { id } = req.params
+  console.log(id)
+  axios.get(`https://api.thedogapi.com/v1/breeds/${id}`)
+    .then((data) => data.data)
+    .then((breeds) => {
+  res.status(200).send(breeds)
+    })
+})
+
+router.post("/dogs", (req, res) => {
+ const body = req.body
+ res.status(201).send(body)
+})
+
+router.get("/temperaments", (req, res) => {
+  axios.get("https://api.thedogapi.com/v1/breeds")
+  .then(res => res.data)
+  .then(temps => {
+    let tempsFound = temps.map(t => t.temperament)
+    res.status(200).send(tempsFound);
+  })
+})
 
 module.exports = router;
